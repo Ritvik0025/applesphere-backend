d64e93a8a13b81e66eb55b0f440ba8f73a99bcbe
Ritvik0025 <ribhu123ritvik@gmail.com>
2026-06-12 15:50:12 +0530

    Initial Spring Boot backend setup

---
+++ src/test/java/com/applesphere/applesphere_backend/ApplesphereBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.applesphere.applesphere_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplesphereBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
